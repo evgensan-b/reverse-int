a5bdf1bafb8a1ef775b0770f6603d55d1fca4d0d
EvgenSan <evgnesan@yandex.ru>
2025-09-01 18:26:59 +0300

    feat: solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const absoluteNumber = Math.abs(n);
+  const numStr = absoluteNumber.toString();
+  const reversedString = numStr.split('').reverse().join('');
+  const result = parseInt(reversedString, 10);
+
+  return result;
 };
